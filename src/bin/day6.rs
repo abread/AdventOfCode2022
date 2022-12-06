9060183a1ea7d0cfc565a8e2c09d74b418490215
André Breda <andrebreda@tecnico.ulisboa.pt>
2022-12-06 12:24:58 +0000

    add day6p2

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,22 +1,35 @@
 use std::io::Read;
 
 fn main() {
-    let input = std::io::stdin().lock().bytes().map(Result::unwrap);
+    let input: Vec<_> = std::io::stdin()
+        .lock()
+        .bytes()
+        .map(Result::unwrap)
+        .collect();
 
-    dbg!("part1", find_packet_start(input));
+    dbg!("part1", find_packet_start(input.iter().copied()));
+    dbg!("part2", find_message_start(input.into_iter()));
 }
 
 fn find_packet_start(input: impl Iterator<Item = u8>) -> usize {
-    let mut ring = [0u8; 4];
+    find_start_n_different_chars::<4>(input)
+}
+
+fn find_message_start(input: impl Iterator<Item = u8>) -> usize {
+    find_start_n_different_chars::<14>(input)
+}
+
+fn find_start_n_different_chars<const N: usize>(input: impl Iterator<Item = u8>) -> usize {
+    let mut ring = [0u8; N];
     let mut idx = 0;
     let mut st = 0;
 
     for b in input {
-        ring[idx % 4] = b;
+        ring[idx % N] = b;
 
         let mut new_st = st + 1;
         for i in 1..=st {
-            if ring[(idx - i) % 4] == b {
+            if ring[(idx - i) % N] == b {
                 new_st = i;
                 break;
             }
@@ -25,7 +38,7 @@ fn find_packet_start(input: impl Iterator<Item = u8>) -> usize {
 
         idx += 1;
 
-        if st == 4 {
+        if st == N {
             return idx;
         }
     }
