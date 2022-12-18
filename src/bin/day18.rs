0337b4f8e94349311d05ff9500e47bdad25c2181
André Breda <andrebreda@tecnico.ulisboa.pt>
2022-12-18 20:34:52 +0000

    day18p2

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashSet, io::stdin, ops::Add, str::FromStr};
+use std::{
+    collections::HashSet,
+    io::stdin,
+    ops::Add,
+    str::FromStr,
+};
 
 fn main() {
     let puzzle = Puzzle::parse(
@@ -9,19 +14,16 @@ fn main() {
     );
 
     // part 1
-    println!(
-        "{}",
-        puzzle
-            .0
-            .iter()
-            .map(|&c| puzzle.count_exposed_sides(c))
-            .sum::<usize>()
-    );
+    println!("{}", puzzle.solve1());
+
+    // part 2
+    println!("{}", puzzle.solve2());
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 struct Coord3(i64, i64, i64);
 
+#[derive(Debug)]
 struct Puzzle(HashSet<Coord3>);
 
 impl Puzzle {
@@ -30,7 +32,93 @@ impl Puzzle {
         Puzzle(cubes)
     }
 
+    fn solve1(&self) -> usize {
+        self.0
+            .iter()
+            .map(|&c| self.count_exposed_sides(c))
+            .sum::<usize>()
+    }
+
+    fn solve2(&self) -> usize {
+        let bounding_box = self.bounding_box();
+        let point_outside_min = bounding_box.0 + Coord3(-1, -1, -1);
+        let point_outside_max = bounding_box.1 + Coord3(1, 1, 1);
+        let bounding_box = bounding_box
+            .extend(point_outside_max)
+            .extend(point_outside_min);
+
+        let mut visited = HashSet::with_capacity(self.0.len() * 6);
+        let mut queue = Vec::new();
+        queue.push((point_outside_min, point_outside_max));
+
+        let mut count = 0;
+        while let Some((comes_from, point)) = queue.pop() {
+            if !bounding_box.contains(point) || visited.contains(&(comes_from, point)) {
+                continue;
+            }
+
+            visited.insert((comes_from, point));
+
+            if self.0.contains(&point) {
+                count += 1;
+            } else {
+                for neigh in point.neighbors().filter(|&n| n != comes_from) {
+                    queue.push((point, neigh));
+                }
+            }
+        }
+
+        count
+    }
+
     fn count_exposed_sides(&self, cube_pos: Coord3) -> usize {
+        cube_pos
+            .neighbors()
+            .filter(|coord| !self.0.contains(coord))
+            .count()
+    }
+
+    fn bounding_box(&self) -> Coord3Range {
+        self.0.iter().copied().fold(
+            Coord3Range::new(self.0.iter().copied().next().unwrap()),
+            Coord3Range::extend,
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Coord3Range(Coord3, Coord3);
+
+impl Coord3Range {
+    fn new(coord: Coord3) -> Self {
+        Coord3Range(coord, coord)
+    }
+
+    fn extend(self, coord: Coord3) -> Self {
+        let Coord3Range(mut min, mut max) = self;
+        min.0 = min.0.min(coord.0);
+        min.1 = min.1.min(coord.1);
+        min.2 = min.2.min(coord.1);
+        max.0 = max.0.max(coord.0);
+        max.1 = max.1.max(coord.1);
+        max.2 = max.2.max(coord.2);
+
+        Coord3Range(min, max)
+    }
+
+    fn contains(&self, coord: Coord3) -> bool {
+        let Coord3Range(min, max) = &self;
+        coord.0 >= min.0
+            && coord.0 <= max.0
+            && coord.1 >= min.1
+            && coord.1 <= max.1
+            && coord.2 >= min.2
+            && coord.2 <= max.2
+    }
+}
+
+impl Coord3 {
+    fn neighbors(self) -> impl Iterator<Item = Self> {
         [
             Coord3(0, 0, 1),
             Coord3(0, 0, -1),
@@ -40,9 +128,7 @@ impl Puzzle {
             Coord3(-1, 0, 0),
         ]
         .into_iter()
-        .map(|delta| delta + cube_pos)
-        .filter(|coord| !self.0.contains(coord))
-        .count()
+        .map(move |delta| self + delta)
     }
 }
 
